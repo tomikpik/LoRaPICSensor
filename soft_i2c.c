69a9a28de27b571dad1c1c1e777bc9ffd2008c8e
pikous <tomas.pikous@gmail.com>
2016-03-20 22:35:57 +0100

    asdasd

--- soft_i2c.c
+++ soft_i2c.c
@@ -1,155 +1,158 @@
-#if defined(__XC)
-    #include <xc.h>         /* XC8 General Include File */
-#elif defined(HI_TECH_C)
-    #include <htc.h>        /* HiTech General Include File */
-#endif
+#include <xc.h>
 
-#include <stdint.h>        /* For uint8_t definition */
-#include <stdbool.h>       /* For true/false definition */
-#include "soft_i2c.h"       /* For true/false definition */
+#include <stdio.h>
+#include <stdlib.h>
 
-// Function Purpose: Set initial values of SCK and SDA pins
-void InitI2C(void)
-{	
-	// Make SDA and SCK pins input initially
-	SDA_DIR = 1;
-	SCK_DIR = 1;
-	
-	// Write zero in output register of SDA and SCK pin
-	SDA = 0;
-	SCK = 0;
+#include "soft_i2c.h"
+
+
+#define _XTAL_FREQ 64000000
+
+void I2C_SDA_SET()   { LATC4 = 1; }
+void I2C_SDA_CLEAR() { LATC4 = 0; }
+
+void I2C_SDA_IN_SET()   { TRISC4 = 1; }
+void I2C_SDA_OUT_SET() { TRISC4 = 0; }
+
+void I2C_SCL_SET()   { LATC3 = 1; }
+void I2C_SCL_CLEAR() { LATC3 = 0; }
+
+void I2C_SCL_OUT_SET() { TRISC3 = 0; }
+
+char I2C_SDA_READ() { return PORTCbits.RC4; }
+
+
+void I2C_Init()
+{
+    I2C_SDA_OUT_SET();
+    I2C_SCL_OUT_SET();
+    I2C_SCL_SET();
+    I2C_SDA_SET();
+    __delay_ms(10);
 }
 
-
-// Function Purpose: I2C_Start sends start bit sequence
-void I2C_Start(void)
+void I2C_Start()
 {
-	SDA = 0;	// Write zero in output register 
-	SCK = 0;	// of SDA and SCK pin
+    I2C_SDA_OUT_SET();
+    //I2C_SCL_OUT_SET();
 
-	Set_SCK_High;				// Make SCK pin high
-	Set_SDA_High;				// Make SDA pin High
-	__delay_us(HalfBitDelay);	// Half bit delay
-	Set_SDA_Low;				// Make SDA Low
-	__delay_us(HalfBitDelay);	// Half bit delay
+    I2C_SDA_SET();
+    I2C_SCL_SET();
+    __delay_us(20);
+
+
+    I2C_SDA_CLEAR();
+    __delay_us(20);
+    I2C_SCL_CLEAR();
+    __delay_us(20);
 }
 
-
-// Function Purpose: I2C_ReStart sends start bit sequence
-void I2C_ReStart(void)
+void I2C_Stop()
 {
-	Set_SCK_Low;				// Make SCK pin low
+    I2C_SDA_OUT_SET();
+    //I2C_SCL_OUT_SET();
 
-	__delay_us(HalfBitDelay/2);	// Data pin should change it's value,
-								// when it is confirm that SCK is low
-	Set_SDA_High;				// Make SDA pin High
-	
-	__delay_us(HalfBitDelay/2);	// 1/4 bit delay
-	Set_SCK_High;				// Make SCK pin high
-	__delay_us(HalfBitDelay/2);	// 1/4 bit delay
-	Set_SDA_Low;				// Make SDA Low
-	__delay_us(HalfBitDelay/2);	// 1/4 bit delay
+    I2C_SCL_SET();
+    __delay_us(20);
+    I2C_SDA_SET();
+    __delay_us(20);
 }
 
-
-//Function : I2C_Stop sends stop bit sequence
-void I2C_Stop(void)
+void I2C_Write_Byte(char byt)
 {
-	Set_SCK_Low;				// Make SCK pin low
-
-	__delay_us(HalfBitDelay/2);	// Data pin should change it's value,
-								// when it is confirm that SCK is low
-	Set_SDA_Low;				// Make SDA pin low
-	
-	__delay_us(HalfBitDelay/2);	// 1/4 bit delay
-	Set_SCK_High;				// Make SCK pin high
-	__delay_us(HalfBitDelay/2);	// 1/4 bit delay
-	Set_SDA_High;				// Make SDA high
-	__delay_us(HalfBitDelay/2);	// 1/4 bit delay
-}
-
-
-
-//Function : I2C_Send_ACK sends ACK bit sequence
-void I2C_Send_ACK(void)
-{
-	Set_SCK_Low;				// Make SCK pin low
-	__delay_us(HalfBitDelay/2);	// Data pin should change it's value,
-								// when it is confirm that SCK is low
-	Set_SDA_Low;				// Make SDA Low
-	__delay_us(HalfBitDelay/2);	// 1/4 bit delay
-	Set_SCK_High;				// Make SCK pin high
-	__delay_us(HalfBitDelay);	// Half bit delay
-}
-
-
-//Function : I2C_Send_NACK sends NACK bit sequence
-void I2C_Send_NACK(void)
-{
-	Set_SCK_Low;				// Make SCK pin low
-	__delay_us(HalfBitDelay/2);	// Data pin should change it's value,
-								// when it is confirm that SCK is low
-	Set_SDA_High;				// Make SDA high
-	__delay_us(HalfBitDelay/2);	// 1/4 bit delay
-	Set_SCK_High;				// Make SCK pin high
-	__delay_us(HalfBitDelay);	// Half bit delay
-}
-
-
-// Function Purpose: I2C_Write_Byte transfers one byte
-bit I2C_Write_Byte(unsigned char Byte)
-{
-	unsigned char i;		// Variable to be used in for loop
-	
-	for(i=0;i<8;i++)		// Repeat for every bit
-	{
-		Set_SCK_Low;		// Make SCK pin low
-		
-		__delay_us(HalfBitDelay/2);	// Data pin should change it's value,
-									// when it is confirm that SCK is low
-
-		if((Byte<<i)&0x80)  // Place data bit value on SDA pin
-			Set_SDA_High;	// If bit is high, make SDA high
-		else				// Data is transferred MSB first
-			Set_SDA_Low;	// If bit is low, make SDA low
-
-		__delay_us(HalfBitDelay/2);	// Toggle SCK pin
-		Set_SCK_High;				// So that slave can
-		__delay_us(HalfBitDelay);	// latch data bit
+    char i;
+    I2C_SDA_OUT_SET();
+    //I2C_SCL_OUT_SET();
+    for(i=0;i<8;i++)
+    {
+        if(byt & 0x80)
+        {
+          I2C_SDA_SET();
+          __delay_us(20);
+        }
+        else
+        {
+          I2C_SDA_CLEAR();
+          __delay_us(20);
+        }
+ 
+        I2C_SCL_SET();
+        __delay_us(20);
+        I2C_SCL_CLEAR();
+        __delay_us(20);
+        byt<<=1;
     }
-		
-	// Get ACK from slave
-	Set_SCK_Low;
-    Set_SDA_High;
-    __delay_us(HalfBitDelay);
-    Set_SCK_High;
-    __delay_us(HalfBitDelay);
+        I2C_SDA_IN_SET();
+        I2C_SCL_SET();
+        __delay_us(20);
+        I2C_SCL_CLEAR();
+        __delay_us(20);
 
-	return SDA;
+    I2C_SDA_OUT_SET();
+    //I2C_SCL_OUT_SET();
+
 }
 
 
-// Function Purpose: I2C_Read_Byte reads one byte
-unsigned char I2C_Read_Byte(void)
-{
-	unsigned char i, RxData = 0;
 
-	Set_SCK_Low; Set_SDA_High; __delay_us(HalfBitDelay); Set_SCK_High;__delay_us(HalfBitDelay/2);
-	RxData |= SDA_READ; RxData = RxData<<1;
-	Set_SCK_Low; Set_SDA_High; __delay_us(HalfBitDelay); Set_SCK_High;__delay_us(HalfBitDelay/2);
-	RxData |= SDA_READ; RxData = RxData<<1;
-	Set_SCK_Low; Set_SDA_High; __delay_us(HalfBitDelay); Set_SCK_High;__delay_us(HalfBitDelay/2);
-	RxData |= SDA_READ; RxData = RxData<<1;
-	Set_SCK_Low; Set_SDA_High; __delay_us(HalfBitDelay); Set_SCK_High;__delay_us(HalfBitDelay/2);
-	RxData |= SDA_READ; RxData = RxData<<1;
-	Set_SCK_Low; Set_SDA_High; __delay_us(HalfBitDelay); Set_SCK_High;__delay_us(HalfBitDelay/2);
-	RxData |= SDA_READ; RxData = RxData<<1;
-	Set_SCK_Low; Set_SDA_High; __delay_us(HalfBitDelay); Set_SCK_High;__delay_us(HalfBitDelay/2);
-	RxData |= SDA_READ; RxData = RxData<<1;
-	Set_SCK_Low; Set_SDA_High; __delay_us(HalfBitDelay); Set_SCK_High;__delay_us(HalfBitDelay/2);
-	RxData |= SDA_READ; RxData = RxData<<1;
-	Set_SCK_Low; Set_SDA_High; __delay_us(HalfBitDelay); Set_SCK_High;__delay_us(HalfBitDelay/2);
-	RxData |= SDA_READ;
-     
-    return RxData;						// Return received byte
+char I2C_Read_Byte(char ack)
+{
+    char i;
+    char val = 0;
+    I2C_SDA_IN_SET();
+    //I2C_SCL_OUT_SET();
+    /*
+    for(i=0;i<8;i++)
+    {
+        val>>=1;
+        I2C_SCL_SET();
+        __delay_us(20);
+        if(I2C_SDA_READ()!=0)
+        {
+            val |= 0x80;
+        }
+        I2C_SCL_CLEAR();
+        __delay_us(20);
+    }*/
+    for(i=0;i<8;i++)
+    {
+        if(i!=0) { val>>=1; }
+        I2C_SCL_SET();
+        __delay_us(4);
+        if(I2C_SDA_READ()!=0)
+        {
+            val |= 0x80;
+        }
+        else
+        {
+            val &= 0x7F;
+   
+        }
+        __delay_us(10);
+        I2C_SCL_CLEAR();
+        __delay_us(20);
+    }
+    
+    I2C_SDA_OUT_SET();
+    //I2C_SCL_OUT_SET();
+
+        if(ack)
+        {
+          I2C_SDA_CLEAR(); 
+          I2C_SCL_SET();
+          __delay_us(20);
+          I2C_SCL_CLEAR();
+          __delay_us(20);
+        }
+        else
+        {
+          I2C_SDA_SET(); 
+          I2C_SCL_SET();
+          __delay_us(20);
+          I2C_SCL_CLEAR();
+          __delay_us(20);
+        }
+   
+    return val;
+
 }
