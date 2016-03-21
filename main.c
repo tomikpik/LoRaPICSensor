238c93af45ed6cb5300563a6e6be75cce9a521f3
pikous <tomas.pikous@gmail.com>
2016-03-21 01:16:03 +0100

    reading illumination works, sending just msb, lsb and crc

--- main.c
+++ main.c
@@ -53,97 +53,85 @@ void getUUID(){
     strcpy(uuid,line); 
 }
 
-void process_data_opt(uint8_t msb, uint8_t lsb){
-    int16_t raw;
-    uint16_t exponent = 0;
-	uint32_t result = 0;
-    uint32_t Illumination = 0;
+void InitRadio(void){
+    wait_5ms(5); 
+    printf("sys reset\r\n");
+    wait_5ms(5); 
+    printf("sys get ver\r\n");
+    read_line(); 
+    printf("radio get sf\r\n");
+    read_line();  
+    printf("radio get bw\r\n");
+    read_line();  
+    printf("radio get cr\r\n");
+    read_line();  
+    printf("radio get prlen\r\n");
+    read_line(); 
+    printf("radio get pwr\r\n");
+    read_line(); 
+    printf("mac pause\r\n");
+    read_line(); 
+           
+    printf("radio set pwr -2\r\n");
+    read_line(); 
+    printf("radio set bw 500\r\n");
+    read_line(); 
+    printf("radio set sf sf7\r\n");
+    read_line(); 
+    printf("radio set cr 4/8\r\n");
+    read_line(); 
+    printf("radio set crc on\r\n");
+    read_line();
     
-    
-    raw = ((msb << 8) | lsb);
-
-	result = raw&0x0FFF;
-	exponent = (raw>>12)&0x000F;
-
-	switch(exponent){
-		case 0: //*0.015625
-			Illumination = result>>6;
-			break;
-		case 1: //*0.03125
-			Illumination = result>>5;
-			break;
-		case 2: //*0.0625
-			Illumination = result>>4;
-			break;
-		case 3: //*0.125
-			Illumination = result>>3;
-			break;
-		case 4: //*0.25
-			Illumination = result>>2;
-			break;
-		case 5: //*0.5
-			Illumination = result>>1;
-			break;
-		case 6:
-			Illumination = result;
-			break;
-		case 7: //*2
-			Illumination = result<<1;
-			break;
-		case 8: //*4
-			Illumination = result<<2;
-			break;
-		case 9: //*8
-			Illumination = result<<3;
-			break;
-		case 10: //*16
-			Illumination = result<<4;
-			break;
-		case 11: //*32
-			Illumination = result<<5;
-			break;
-	}
-    printf("> %d\r\n",Illumination);
-
+    getUUID();
+     
+    srand((int)strtol(uuid, NULL, 0));
+    int delay = 2000+(rand()%1000);
+    printf("radio set wdt %d\r\n",delay);
+    read_line(); 
 }
 
-
-void main(void) {
-    SYSTEM_Initialize(); 	
-    INTERRUPT_GlobalInterruptEnable();  
-	INTERRUPT_PeripheralInterruptEnable();
+void InitSensor(void){
     ANSELCbits.ANSC4=0;
-
-    I2C_Init();
-    wait_5ms(4);  
-    I2C_Stop();
-    wait_5ms(4);  
-    
-    
+    I2C_Init();  
     I2C_Start();
     I2C_Write_Byte(0x88);
     I2C_Write_Byte(0x01);
     I2C_Write_Byte(0xC4);
     I2C_Write_Byte(0x10);
     I2C_Stop();
-    
-    __delay_ms(1); 
+}
 
+void main(void) {
+    SYSTEM_Initialize(); 	
+    INTERRUPT_GlobalInterruptEnable();  
+	INTERRUPT_PeripheralInterruptEnable();
+    
+    InitRadio();
+    InitSensor();
+    
+    
     while(1){
         
         I2C_Start();
         I2C_Write_Byte(0x88);
-        I2C_Write_Byte(0x7E);
+        I2C_Write_Byte(0x00);
         I2C_Start();
         I2C_Write_Byte(0x89);
-        uint8_t msb = I2C_Read_Byte();
-        uint8_t lsb = I2C_Read_Byte();
+        uint8_t msb = I2C_Read_Byte(1);
+        uint8_t lsb = I2C_Read_Byte(0);
         I2C_Stop();
-        printf("%x %x\r\n",msb,lsb);
-        process_data_opt(msb,lsb);
+        uint8_t crc = msb ^ lsb;
+        
+        
+        //delay = 2000+(rand()%1000);
+        printf("radio tx 41%s%02X%02X%02X%02X0000\r\n",uuid,inc++,msb,lsb,crc); 
+        read_line(); 
+        read_line();
+        
         
         wait_5ms(200);        
-        
+        wait_5ms((rand()%4));
     }
   
     return;
